4701b54f3874d4cdec0a86f43b4254ab7c9cd67a
xb0309 <379774535@qq.com>
2018-02-24 03:50:14 +0000

    Say what

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hello Laravel! - by Summer</div>
+                <div class="title">Hello Laravel! - by xb0309</div>
             </div>
         </div>
     </body>
